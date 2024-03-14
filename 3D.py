d45cd1a384690acdb1e533be75369ace2cf63c97
Aidan W <143602720+aido77@users.noreply.github.com>
2024-03-14 12:25:28 +0000

    Update 3D.py

--- 3D.py
+++ 3D.py
@@ -1,15 +1,26 @@
 import pyvista as pv
-from pyvista import examples
+#imports pyvista, the rendering thing
 from models import *
+#imports the models, earth and satellites
 
 pl = pv.Plotter()
+#creates a 3d space for the models to be rendered in
 pl.add_mesh(body, color='silver', specular=1.0, specular_power=10)
+#renders the body of the satellite {silver}
 pl.add_mesh(wing1, color='lightblue', specular=1.0, specular_power=10)
+#renders wing1 of the satellite
 pl.add_mesh(wing2, color='lightblue', specular=1.0, specular_power=10)
-
-cubemap = examples.download_cubemap_space_16k()
-_ = pl.add_actor(cubemap.to_skybox())
-pl.set_environment_texture(cubemap, True)
+#renders wing2 of the satellite
 
 pl.add_mesh(earth, texture=earth_texture, smooth_shading=False)
+#adds the earth, with the earth texture
+
+cubemap = examples.download_cubemap_space_16k()
+#downloads the "space" background {from NASA}
+_ = pl.add_actor(cubemap.to_skybox())
+#makes it an "actor" so it can be added to the skybox
+pl.set_environment_texture(cubemap, True)
+#adds it as a "cubemap" texture
+
 pl.show()
+#renders the plotted items
