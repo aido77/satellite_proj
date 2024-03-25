1a0e9133c5a9f599bfa10e2592cd1b722b1b4d75
Aidan W <143602720+aido77@users.noreply.github.com>
2024-03-25 12:36:38 +0000

    Create sat_database.py
    simple little small satellite dictionary that has a variety of different kinds of satellites

---
+++ sat_database.py
@@ -0,0 +1,10 @@
+sat_dict = {}
+
+sat_dict["Space & Earth Science"] = "https://celestrak.org/NORAD/elements/gp.php?GROUP=science&FORMAT=tle"
+sat_dict["Weather"] = "https://celestrak.org/NORAD/elements/gp.php?GROUP=weather&FORMAT=tle"
+sat_dict["Space Stations"] = "https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=tle"
+sat_dict["Starlink: ~5000 SATELLITES: RUN AT OWN RISK"] = "https://celestrak.org/NORAD/elements/gp.php?GROUP=starlink&FORMAT=tle"
+sat_dict["Amateur Radio"] = "https://celestrak.org/NORAD/elements/gp.php?GROUP=amateur&FORMAT=tle"
+sat_dict["Search & Rescue"] = 'https://celestrak.org/NORAD/elements/gp.php?GROUP=sarsat&FORMAT=tle'
+sat_dict["Earth Resources"] = 'https://celestrak.org/NORAD/elements/gp.php?GROUP=resource&FORMAT=tle'
+sat_dict["Navy Navigation System"] = 'https://celestrak.org/NORAD/elements/gp.php?GROUP=nnss&FORMAT=tle'
