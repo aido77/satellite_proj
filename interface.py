b1623775041e21bdef72710b09cf3bb424687072
Aidan W <143602720+aido77@users.noreply.github.com>
2024-03-28 13:16:57 +0000

    Update interface.py
    now has learning mode

--- interface.py
+++ interface.py
@@ -3,13 +3,18 @@ from tkinter import ttk
 from render import *
 from sat_database import *
 from attributes import *
-
+from questiongenerator import *
+import tkinter as tk
+from tkinter import *
+import time
 
 root = Tk()
 root.title("Satellite Interface")
 root.minsize(600, 600)
 title = Label(root, text="Welcome to the Satellite Interface!")
 title.pack()
+global entry
+entry = tk.Entry(root)
 #initialises the satellite 
 def openSatelliteAttributeDisplay(URL2):
     # Toplevel object which will 
@@ -41,7 +46,8 @@ def openSatelliteAttributeDisplay(URL2):
     for i in range(len(attribute_list)):
         tree.insert('', 'end', text=i, values=(attribute_list[i]))
     tree.pack()
-        #render(URL2)
+    time.sleep(10)
+    render(URL2)
     
 def select():
     init = Button(root, text="BEGIN", command=lambda: openSatelliteAttributeDisplay(URL2))
@@ -57,10 +63,14 @@ def select():
     print("coordslen =",len(coords))
     init.pack()
 
-
-
 satellite_filter = Listbox(root)
 satellite_filter.pack()
+
+def drop():
+    title.forget()
+    satellite_filter.forget()
+    filter_select.forget()
+
 
 for item in sat_dict:
     satellite_filter.insert(END, item)
@@ -68,5 +78,104 @@ for item in sat_dict:
 filter_select = Button(root, text='SELECT', command=select)
 filter_select.pack(pady= 10)
 
+def validate(question_count):
+    tempvar = StringVar()
+    temp_q = question_list[question_count]
+    tempvar.set(temp_q[1])
+    incorrect1 = tk.Label(text='Sorry, that is incorrect, the correct answer is ',fg='red')
+    incorrect2 = tk.Label(textvariable=tempvar,fg='red')
+    correct = tk.Label(text='Correct!',fg='green')
+    attempt = entry.get()
+    correct.forget()
+    incorrect1.forget()
+    incorrect2.forget()
 
+    if attempt == temp_q[1]:
+        correct.pack()
+        entry.delete(0, tk.END)
+        if question_count == 0:
+            var.set(question_full[question_count])
+            validate0.forget()
+            validate1.pack()
+        elif question_count == 1:
+            var.set(question_full[question_count])
+            question.pack()
+            validate1.forget()
+            validate2.pack()
+        elif question_count == 2:
+            var.set(question_full[question_count])
+            question.pack()
+            validate2.forget()
+            validate3.pack()
+        elif question_count == 3:
+            var.set(question_full[question_count])
+            question.pack()
+            validate3.forget()
+            validate4.pack()
+        elif question_count == 4:
+            question.forget()
+            entry.forget()
+            validate4.forget()
+            tryagain = tk.Button(text='You have finished your 5 questions: do get some more!')
+            tryagain.pack()
+    else:
+        incorrect1.forget()
+        incorrect2.forget()
+        correct.forget()
+        incorrect1.pack()
+        incorrect2.pack()
+        entry.delete(0, tk.END)
+        if question_count == 0:
+            var.set(question_full[question_count])
+            question.pack()
+            validate0.forget()
+            validate1.pack()
+        elif question_count == 1:
+            var.set(question_full[question_count])
+            question.pack()
+            validate1.forget()
+            validate2.pack()
+        elif question_count == 2:
+            var.set(question_full[question_count])
+            question.pack()
+            validate2.forget()
+            validate3.pack()
+            
+        elif question_count == 3:
+            var.set(question_full[question_count])
+            question.pack()
+            validate3.forget()
+            validate4.pack()
+        elif question_count == 4:
+            question.forget()
+            entry.forget()
+            validate4.forget()
+            tryagain = tk.Button(text='You have finished your 5 questions: do get some more!')
+            tryagain.pack()
+             
+           
+def learningmode_init():
+    question_formulate()
+    drop()
+    print(question_list)
+    title = tk.Label(text="WELCOME TO LEARNING MODE")
+    info = tk.Label(text='Learning Mode is a great way to improve your circular motion and gravitational field question technique/nEach example is taken from real-time satellites in orbit.')
+    title.pack()
+    info.pack()
+    global validate0, validate1,validate2,validate3,validate4,question,var
+    validate0 = tk.Button(root,text='Validate Answer',command=lambda: validate(0))
+    validate1 = tk.Button(root,text='Validate Answer',command=lambda: validate(1))
+    validate2 = tk.Button(root,text='Validate Answer',command=lambda: validate(2))
+    validate3 = tk.Button(root,text='Validate Answer',command=lambda: validate(3))
+    validate4 = tk.Button(root,text='Validate Answer',command=lambda: validate(4))
+    var = StringVar()
+    var.set(question_full[0])
+    question = tk.Label(root,textvariable= var)
+    question.pack()
+    entry.pack()
+    validate0.pack()
+    #validate0.pack()
+    root.mainloop()
+learningmode = Button(text='Click Here for Learning Mode',command=lambda: learningmode_init())
+learningmode.pack()
 root.mainloop()
