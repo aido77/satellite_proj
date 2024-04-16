812d5cf85b3e176418730b39dbd15a3b7a5bd209
Aidan W <143602720+aido77@users.noreply.github.com>
2024-04-16 09:30:03 +0100

    Update render.py

--- render.py
+++ render.py
@@ -6,7 +6,6 @@ from attributes import *
 import skyfield
 from skyfield.api import EarthSatellite
 from skyfield.api import load, wgs84
-from numpy import sqrt
 
 pl = pv.Plotter()
 #creates a space for the 3d models to be plotted
@@ -16,17 +15,20 @@ def satellite_model(x,y,z):
     #creates the "body" of the satellite with specified center
     wing1 = pv.Cube(x_length=450, y_length=148, z_length=52.5,center=(x,y,z))
     #creates one "wing" of the satellite
-    pl.add_mesh(body, color='silver', show_edges=10, edge_color='black', specular=1.0, specular_power=10)
-    pl.add_mesh(wing1, color='lightblue', show_edges=10, edge_color = 'black', specular=1.0, specular_power=10)
+    pl.add_mesh(body, color='silver', show_edges=5, edge_color='darkgrey', specular=1.0, specular_power=10)
+    pl.add_mesh(wing1, color='lightblue', show_edges=5, edge_color = 'darkgrey', specular=1.0, specular_power=10)
     #adds the object to the 3d space
-def close_window(flag):
-    if flag == True:
-        pl.close()
+
 
 def render(URL2):
-    tempData = load.tle_file(URL2, reload=True)
-    print("tempdatalen =", len(tempData))
+    earth = examples.planets.load_earth(radius=6371)
+    #creates the earth with it's set radius
+    earth_texture = examples.load_globe_texture()
+    #imports earth texture
+    pl.add_mesh(earth, texture=earth_texture, smooth_shading=False)
+    #adds the earth, with the earth texture
 
+    tempData = load.tle_file(URL2, reload=True)
     attribute_refresh(len(tempData),URL2)
     numb_Sat = len(coords)
     count_sat = 0
@@ -43,17 +45,6 @@ def render(URL2):
         z = temp_satCoord[2]
         satellite_model(x,y,z)
         count_sat = count_sat + 1
-    print(numb_Sat)
-
-    earth = examples.planets.load_earth(radius=6378.1)
-    #creates the earth with it's set radius
-    earth_texture = examples.load_globe_texture()
-    #imports earth texture
-
-    pl.add_mesh(earth, texture=earth_texture, smooth_shading=False)
-    #adds the earth, with the earth texture
-    pl.add_checkbox_button_widget(close_window,value = False)
-
     cubemap = examples.download_cubemap_space_16k()
     #downloads the "space" background {from NASA}
     _ = pl.add_actor(cubemap.to_skybox())
