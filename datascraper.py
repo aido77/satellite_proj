5f4c61fd621d6063015a3f8175435a8da56c4331
Aidan W <143602720+aido77@users.noreply.github.com>
2024-04-16 09:27:59 +0100

    Update datascraper.py

--- datascraper.py
+++ datascraper.py
@@ -1,12 +1,9 @@
-#note THIS NOW ONLY CONTAINS THE "REQUESTS" PART OF THE CODE
-import pip._vendor.requests #allows to scrape data from websites
 import skyfield
 from skyfield.api import EarthSatellite
 from skyfield.api import load, wgs84
 
+global URL
 URL = 'http://celestrak.org/NORAD/elements/stations.txt' #this is an "offline" text so i dont get banned again
-bigData = load.tle_file(URL) 
-#print('Loaded', len(bigData), 'satellites')
-    # NORAD contains the latest, up to date TLE data that is non predicted, so items are not constantly changing [updated at 4pm UTC]
-#pulls the text from the page and stores it in "bigData"
-#print(bigData[2])
+# NORAD contains the latest, up to date TLE data that is non predicted, so items are not constantly changing [updated at 4pm UTC]
+bigData = load.tle_file(URL)
+#initialises bigData
